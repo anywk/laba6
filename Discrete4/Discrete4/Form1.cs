ed5127fc45431abf8d0a4666a4fcf141d5614edc
kamaek <o-grank@yandex.ru>
2017-03-06 01:28:48 +0200

    first commit

---
+++ Discrete4/Discrete4/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discrete4
+{
+    public partial class Form1 : Form
+    {
+
+        private bool[][] adjacencyMatrix = new bool[8][] {
+            new bool[] {false, false, true, true, false, false, false, true},
+            new bool[] {false, false, false, false, true, false, false, false},
+            new bool[] {false, false, false, false, true, true, false, true},
+            new bool[] {false, false, false, false, false, false, true, false},
+            new bool[] {false, false, false, false, false, true, true, false},
+            new bool[] {false, false, false, false, false, false, false, true},
+            new bool[] {false, false, false, false, false, false, false, false},
+            new bool[] {false, false, false, false, false, false, false, false}
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 8;
+            fillTable(adjacencyMatrix);
+        }
+
+        private void button_execute_Click(object sender, EventArgs e)
+        {
+            IDictionary<int, int> grandiFunctions = new Grandi().execute(readDataFromTable());
+
+            dataGridView1.Columns[dataGridView1.Columns.Count - 1].Name = "grandi value";
+            
+            for (int i = 0, grandiFunction; i < grandiFunctions.Count; i++)
+            {
+                grandiFunctions.TryGetValue(i, out grandiFunction);
+                dataGridView1.Rows[i].Cells[dataGridView1.Columns.Count - 1].Value = grandiFunction;
+            }
+        }
+
+        private bool[][] readDataFromTable()
+        {
+            int verticesCount = dataGridView1.Columns.Count;
+            bool[][] matrix = new bool[verticesCount][];
+            for (int i = 0; i < verticesCount; i++)
+            {
+                matrix[i] = new bool[verticesCount];
+                for (int j = 0; j < verticesCount; j++)
+                {
+                    matrix[i][j] = bool.Parse(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                }
+            }
+            return matrix;
+        }
+
+        private void fillTable(bool[][] adjacencyMatrix)
+        {
+            for (int i = 0; i < adjacencyMatrix.Count(); i++)
+            {
+                dataGridView1.Rows.Add();
+                for (int j = 0; j < adjacencyMatrix.Count(); j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = adjacencyMatrix[i][j].ToString();
+                }
+            }
+        }
+
+    }
+}
---
+++ Discrete4/Discrete4/Grandi.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discrete4
+{
+    class Grandi
+    {
+        public IDictionary<int, int> execute(bool[][] adjacencyMatrix)
+        {
+            int verticesCount = adjacencyMatrix.Count();
+            IDictionary<int, int> grandiFunctions = new Dictionary<int, int>();
+
+            for (int vertexIndex = 0; vertexIndex < verticesCount; vertexIndex++)
+            {
+                if (!isExistsTransitionsFrom(vertexIndex, adjacencyMatrix))
+                {
+                    grandiFunctions.Add(vertexIndex, 0);
+                }
+            }
+
+            while (grandiFunctions.Count < verticesCount)
+            {
+                for (int vertexIndex = 0; vertexIndex < verticesCount; vertexIndex++)
+                {
+                    if (!grandiFunctions.ContainsKey(vertexIndex) && isAllGrandiFunctionsKnown(vertexIndex, grandiFunctions, adjacencyMatrix))
+                    {
+                        int grandiFunctionValue = 0;
+                        IList<int> grandiValuesForTransitions = new List<int>();
+                        
+                        for (int transitionVertixIndex = 0; transitionVertixIndex < verticesCount; transitionVertixIndex++)
+                        {
+                            if (adjacencyMatrix[vertexIndex][transitionVertixIndex])
+                            {
+                                if (grandiFunctions.TryGetValue(transitionVertixIndex, out grandiFunctionValue))
+                                {
+                                    grandiValuesForTransitions.Add(grandiFunctionValue);
+                                }
+                            }
+                        }
+
+                        grandiFunctionValue = findFirstNonnegativeExcept(grandiValuesForTransitions);
+
+                        grandiFunctions[vertexIndex] = grandiFunctionValue;
+                    }
+                }
+            }
+
+            return grandiFunctions;
+        }
+
+        private bool isExistsTransitionsFrom(int vertexIndex, bool[][] adjacencyMatrix)
+        {
+            foreach (bool transitionExists in adjacencyMatrix[vertexIndex])
+            {
+                if (transitionExists)
+                    return true;
+            }
+
+            return false;
+        }
+
+
+        private bool isAllGrandiFunctionsKnown(int vertexIndex, IDictionary<int, int> grandiFunctions, bool[][] adjacencyMatrix)
+        {
+            for (int i = 0; i < adjacencyMatrix.Count(); i++)
+            {
+                if (adjacencyMatrix[vertexIndex][i] && !grandiFunctions.ContainsKey(i))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private int findFirstNonnegativeExcept(IList<int> values)
+        {
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                if (!values.Contains(i))
+                {
+                    return i;
+                }
+            }
+
+            throw new Exception("Illegal state");
+        }
+
+    }
+}
